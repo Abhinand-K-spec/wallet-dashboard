7dfd7d876a14538aa2f0f28ede415f0567da17c3
Abhinand k <abhinnanndk@gmail.com>
2026-06-24 13:06:59 +0530

    login error 2 wrong api

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+let backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5001/api';
+if (backendUrl && !backendUrl.endsWith('/api') && !backendUrl.endsWith('/api/')) {
+  backendUrl = backendUrl.replace(/\/$/, '') + '/api';
+}
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5001/api',
+  baseURL: backendUrl,
 });
 
 api.interceptors.request.use((config) => {
